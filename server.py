beed6c43f8133afb280eeeeef2cfff258b4661d7
Krishna  Karunaharan <karunah6@p158.ecf.utoronto.ca>
2018-09-27 19:15:47 -0400

    recieving data for sumbit() function

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
 from bottle import route, run, request
+import json
+
+myDict = {}
+
 @route('/submit', method ='POST')
-def hello():    
-    x = request.body.readlines()
-    print (x)
+def submit():    
+	line = request.body.read()
+	inputData = json.loads(line)
+	for key in inputData:
+		myDict[key] = myDict.get(key, 0) + inputData[key]
+
+	print myDict
+
+@route('/history', method ='get')
+def history():
+
+	return history
 
 run(host='localhost', port=8080, debug=True)
