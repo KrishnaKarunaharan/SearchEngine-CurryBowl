8a5a2bfc9339edd52194535384977b608c817ab1
Gaurav Kishore <gaurav.kishore@mail.utoronto.ca>
2018-11-11 18:00:35 -0500

    Fixed ip address

--- server.py
+++ server.py
@@ -22,11 +22,12 @@ recent = []
 users = {}
 current_user = UserData("empty", "empty", "empty")
 
-ip = 'localhost'
-port = 8080
+ip = '0.0.0.0'
+port = 80
 SCOPES = ['https://www.googleapis.com/auth/plus.me', 'https://www.googleapis.com/auth/userinfo.email']
 
-base_url = "http://ec2-18-232-120-67.compute-1.amazonaws.com"
+base_url = "http://" + ip + ":" + str(port)
+
 @route('/<filename>')
 def file(filename):
 	return static_file(filename, root='')
